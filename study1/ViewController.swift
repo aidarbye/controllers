fae05bd63f324acb1fcbc1f895fed45505df3ab9
Aidar Nurkin <aidar.nurkin@icloud.com>
2023-01-19 16:34:33 +0600

    -, branch0

--- study1/ViewController.swift
+++ study1/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mainLabel: UILabel!
+    @IBOutlet var segmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        segmentedControl.insertSegment(withTitle: "Third", at: 2, animated: false)
+        mainLabel.text = ""
+        mainLabel.font = mainLabel.font.withSize(30)
+        mainLabel.numberOfLines = 2
+        mainLabel.textAlignment = .center
     }
-
+    
+    @IBAction func segmentedControlAction(_ sender: Any) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            mainLabel.text = "Selected index is 0"
+            mainLabel.textColor = .red
+        case 1:
+            mainLabel.text = "Selected index is 1"
+            mainLabel.textColor = .yellow
+        case 2:
+            mainLabel.text = "Selected index is 2"
+            mainLabel.textColor = .brown
+            
+        default: break
+        }
+    }
+    
 
 }
 
