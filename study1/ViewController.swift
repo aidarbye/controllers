d7e004bfa80bd0b210fc0e9c4914f260fba1aa63
Aidar Nurkin <aidar.nurkin@icloud.com>
2023-01-19 16:19:46 +0600

    Initial Commit

---
+++ study1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  study1
+//
+//  Created by Айдар Нуркин on 19.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
